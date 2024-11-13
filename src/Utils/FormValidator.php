15ccd7f99bb50978586315cdc4bb74ac7fa4c323
Shadow Walker <franklynpeter2006@gmail.com>
2024-11-13 12:56:47 +0100

    update FormValidator utility class

--- src/Utils/FormValidator.php
+++ src/Utils/FormValidator.php
@@ -7,6 +7,41 @@ namespace App\Utils;
 abstract class FormValidator
 {
 
+    private const FORM_FIELDS = [
+        'username'  => 'validateUsername',
+        'email'     => 'validateEmail',
+        'password'  => 'validatePassword',
+        'password-confirm' => 'validatePasswordConfirm',
+        'experience-level' => 'validateExperienceLevel'
+    ];
+
+    private const VALID_PASSWORD_REGEX = [
+        [
+            'regex'     => '^.{8,}',
+            'message'   => 'Password must be at least 8 characters long'
+        ],
+        [
+            'regex'     => '^(?=.*[A-Za-z]).+$',
+            'message'   => 'Password must contain at least one character'
+        ],
+        [
+            'regex'     => '^(?=.*[A-Z]).+$',
+            'message'   => 'Password must contain at least one uppercase letter'
+        ],
+        [
+            'regex'     => '^(?=.*[a-z]).+$',
+            'message'   => 'Password must contain at least one lowercase letter'
+        ],
+        [
+            'regex'     => '^(?=.*\d).+$',
+            'message'   => 'Password must contain at least one digit'
+        ],
+        [
+            'regex'     => '^(?=.*[!@#$%^&*(),.?":{}|<>]).+$',
+            'message'   => 'Password must contain at least one special character'
+        ]
+    ];
+
     public static function sanitiseData(string $data): string
     {
         return htmlspecialchars(
@@ -39,7 +74,7 @@ abstract class FormValidator
         ];
     }
 
-    public static function validateUsername(string &$username): array
+    public static function validateUsername(string $username): array
     {
         $username = self::sanitiseData($username);
         $validationType = 'username';
@@ -61,7 +96,7 @@ abstract class FormValidator
         return self::getValidationSuccessInfo($validationType);
     }
 
-    public static function validateEmail(string &$email): array
+    public static function validateEmail(string $email): array
     {
         if (! ($email = filter_var($email, FILTER_SANITIZE_EMAIL))) {
             return self::getValidationErrorInfo('email', 'Invalid email address');
@@ -70,12 +105,46 @@ abstract class FormValidator
         return self::getValidationSuccessInfo('email');
     }
 
-    public static function validateExperienceLevel(int &$experienceLevel): array
+    public static function validateExperienceLevel(string|int $experienceLevel): array
     {
+        $experienceLevel = (int) $experienceLevel;
         if ($experienceLevel < 1|| $experienceLevel > 3) {
             return self::getValidationErrorInfo('experience-level', 'Invalid experience level');
         }
 
         return self::getValidationSuccessInfo('experience-level');
+    }
+
+    public static function validatePassword(string $password): array
+    {
+        foreach(self::VALID_PASSWORD_REGEX as $regexConfig) {
+            if (! preg_match("/{$regexConfig['regex']}/", $password)) {
+                return self::getValidationErrorInfo('password', $regexConfig['message']);
+            }
+        }
+
+        return self::getValidationSuccessInfo('password');
+    }
+
+    public static function validatePasswordConfirm(string $password, string $passwordConfirm): array
+    {
+        return $password === $passwordConfirm ?
+            self::getValidationSuccessInfo('password-confirm') :
+            self::getValidationErrorInfo('password', 'Passwords not match')
+        ;
+    }
+
+    public static function validateAll(array $formData): array
+    {
+        $result = [];
+
+        foreach ($formData as $field => $value) {
+            $result[$field] = call_user_func_array(
+                [self::class, self::FORM_FIELDS[$field]], 
+                is_array($value) ? $value : [$value]
+            );
+        }
+
+        return $result;
     }
 }
